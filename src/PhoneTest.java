c0f29b05569a8ea2247e8c2ac69ba023e66a8fa6
WinterWinner25 <3408939491@qq.com>
2026-09-22 14:01:36 +0800

    类和对象

---
+++ src/PhoneTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PhoneTest {
+    public static void main(String[] args) {
+        Phone p=new Phone();
+
+        p.brand="小米";
+        p.price=1999.99;
+
+        System.out.println(p.brand);
+        System.out.println(p.price);
+
+        p.call();
+        p.playGame();
+
+        Phone p2=new Phone();
+        p2.brand="苹果";
+        p2.price=1999.99;
+
+        System.out.println(p2.brand);
+        System.out.println(p2.price);
+    }
+}
